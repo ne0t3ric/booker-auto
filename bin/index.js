6c22ef70e477b409b6ca6a59a97cdd5ac09d9923
MetEric <kh.ericr@gmail.com>
2019-10-24 08:01:25 +0200

    Refactor: separate bin from lib parts, add PadelDate, rename DayPicker to DatePicker

--- bin/index.js
+++ bin/index.js
@@ -1,34 +1,18 @@
 const puppeteer = require('puppeteer')
-const NavigateToLoginHandler = require('./../lib/handlers/NavigateToLogin/NavigateToLoginHandler')
-const LoginFormHandler = require('./../lib/handlers/LoginForm/LoginFormHandler')
-const NavigateToSchedulesHandler = require('./../lib/handlers/NavigateToSchedules/NavigateToSchedulesHandler')
-const DayPickerHandler = require('./../lib/handlers/DayPicker/DayPickerHandler')
-const SchedulePickerHandler = require('./../lib/handlers/SchedulePicker/SchedulePickerHandler')
-const BookingFormHandler = require('../lib/handlers/BookingForm/BookingFormHandler')
+const PadelBooking = require('./../lib')
 
 ;(async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', '--proxy-bypass-list=<-loopback>']})
 
   try{
-    const day = '25/10/2019'
-    const time = '12:00'
-    const start = new NavigateToLoginHandler()
-    const loginForm = new LoginFormHandler()
-    const navigateToSchedules = new NavigateToSchedulesHandler()
-    const dayPicker = new DayPickerHandler(day)
-    const schedulePicker = new SchedulePickerHandler(time)
-    const bookingForm = new BookingFormHandler()
-
-    start.setNext(loginForm)
-    loginForm.setNext(navigateToSchedules)
-    navigateToSchedules.setNext(dayPicker)
-    dayPicker.setNext(schedulePicker)
-    schedulePicker.setNext(bookingForm)
-
+    const date = '2019-10-25T21:00'
+    const padelBooking = new PadelBooking({
+      date: date
+    })
     const page = await browser.newPage()
-    await page.setViewport({width: 1000, height: 1000}); // <-- add await here so it sets viewport after it creates the page
-    
-    await start.handle(page)
+    await page.setViewport({ width: 1000, height: 1000 })
+  
+    await padelBooking.book(page)
    } catch (err) {
     console.error(err.message);
   } finally {
@@ -38,7 +22,9 @@ const BookingFormHandler = require('../lib/handlers/BookingForm/BookingFormHandl
   process.exit(22);
 })()
 
+//Security timeout
+const timeout = 20*1000
 setTimeout((function() {
     console.error('Timeout execeed');
     return process.exit(22);
-}), 20000);
+}), timeout);
